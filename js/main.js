6b717a016edaa89ca6e648ad6634019bf1dbb883
imhotkp <singhkaushlendra1990@gmail.com>
2025-04-06 22:52:49 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Scroll reveal animation (if you want to add more interactivity later)
+document.addEventListener("DOMContentLoaded", function () {
+    const hero = document.querySelector('.hero-text');
+    hero.classList.add('animate__animated', 'animate__fadeInLeft');
+  });
+  
